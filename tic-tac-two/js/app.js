65a4d9075edffcae643b23ccaa3477afded9041b
Venom9605 <148380797+Venom9605@users.noreply.github.com>
2025-02-28 14:07:35 +0200

    board visualized

---
+++ tic-tac-two/js/app.js
@@ -0,0 +1,14 @@
+﻿import * as UI from "./ui.js";
+import { GameBrain } from "./game.js";
+
+let h1 = document.createElement("h1");
+h1.innerHTML = "Tic Tac Two";
+document.body.appendChild(h1);
+
+let game = new GameBrain();
+
+game.makeAMove(1, 1);
+game.makeAMove(0, 0);
+
+let board = UI.getBoard(game.board);
+document.body.appendChild(board);
